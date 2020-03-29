faa70b581c8d4e70f5c85508a7acb475b85dd720
Simon Chenery <simoc@users.sourceforge.net>
2020-03-29 17:50:52 +0200

    Add more tests to MatrixTester test fixture

--- test2.cpp
+++ test2.cpp
@@ -10,6 +10,8 @@ protected:
 		std::cout << "SetUp" << std::endl;
 		m_identity = Matrix<int>({{1, 0}, {0, 1}});
 		m_zeros = Matrix<int>({{0, 0}, {0, 0}});
+		m_dimension3 = Matrix<int>({{11, 2, 13}, {7, 4, 2}, {0, 9, 3}});
+		m_missing_column = Matrix<int>({{8, 4, 6}, {16, 9, 7}, {4, 1}});
 	}
 
 	void TearDown() override
@@ -19,6 +21,8 @@ protected:
 
 	Matrix<int> m_identity;
 	Matrix<int> m_zeros;
+	Matrix<int> m_dimension3;
+	Matrix<int> m_missing_column;
 };
 
 TEST_F(MatrixTester, IsIdentity)
@@ -27,3 +31,9 @@ TEST_F(MatrixTester, IsIdentity)
   EXPECT_FALSE(m_zeros.isIdentity());
 }
 
+TEST_F(MatrixTester, IsValid)
+{
+  EXPECT_TRUE(m_identity.isValid());
+  EXPECT_FALSE(m_missing_column.isValid());
+  EXPECT_FALSE(m_identity.add(m_dimension3).isValid());
+}
